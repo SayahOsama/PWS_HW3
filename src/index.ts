72299511bb34e66b492dfbc62e5c8fb4848f21b2
Osama Sayah <osama.sayah123@gmail.com>
2024-03-18 23:59:50 +0200

    Revert "users API"
    This reverts commit 1cc8faa7a4996d19c7b0cfb470822d031b8eec79.

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,87 @@
 import { createServer, IncomingMessage, ServerResponse } from "http";
 import * as mongoose from "mongoose";
 import * as dotenv from "dotenv";
-import { createOrder, createUser, deleteOrder, getOrders, getUserByIdOrName, mainRoute, updatePrivileges } from "./routes.js";
+
+// import with .js, and not ts.
+// for more info: https://devblogs.microsoft.com/typescript/announcing-typescript-4-7/#type-in-package-json-and-new-extensions
+import { getExample, mainRoute, createRoute,updatePrivileges, createEvent, deleteEvent, updateEvent, getEventsByOrganizer, getEventsByIdOrCategory } from "./routes.js";
+import { CREATE_EVENT, GET_SEGEL, LOGIN, MAIN_ROOT, PERMISSION, SIGNUP, EVENTS_BY_ORGANIZER } from "./const.js";
+import { loginRoute, signupRoute } from "./auth.js";
 
 // For environment-variables
 dotenv.config();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 // Connect to mongoDB
-const dbURI = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@pws.jqme9mr.mongodb.net/Final_Project`;
+const dbURI = `mongodb+srv://OsamaSayah:${process.env.DBPASS}@pws.jqme9mr.mongodb.net/PWS-HW3`;
 await mongoose.connect(dbURI);
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  
-  if (req.url.match(/\/api\/user\/orders\/[\w=&?]+/)) {
+  const route = createRoute(req.url, req.method);
+  if (req.url.match(/\/api\/event\/organizer\/[\w=&?]*/)) {
     if(req.method === "GET"){
-      getOrders(req,res);
+      getEventsByOrganizer(req, res);
       return;
     }
-    if(req.method === "POST"){
-      createOrder(req,res);
-      return;
-    }
+  }
+  if (req.url.match(/\/api\/event\/[\w=&?]*/)) {
     if(req.method === "DELETE"){
-      deleteOrder(req,res);
+      deleteEvent(req, res);
       return;
     }
-  }
-
-  if (req.url.match(/\/api\/user\/permissions/)) {
     if(req.method === "PUT"){
-      updatePrivileges(req,res);
+      updateEvent(req, res);
       return;
     }
-  }
-
-  if (req.url.match(/\/api\/user\/\w+/)) {
     if(req.method === "GET"){
-      getUserByIdOrName(req,res);
-      return;
-    }
-  }
-
-  if (req.url.match(/\/api\/user/)) {
-    if(req.method === "GET"){
-      mainRoute(req,res);
+      getEventsByIdOrCategory(req, res);
       return;
     }
     if(req.method === "POST"){
-      createUser(req,res);
+      createEvent(req, res);
       return;
     }
   }
-  
-  res.statusCode = 404;
-  res.end("route does not exist");
+  if (req.url.match(/\/api\/login\?[\w=&?]*/) && req.method === "POST") {
+    loginRoute(req, res);
+    return;
+  }
 
+  if (req.url.match(/\/api\/signup\?[\w=&]*/) && req.method === "POST") {
+    signupRoute(req, res);
+    return;
+  }
+
+  if (req.url.match(/\/api\/permission\?[\w=&]*/) && req.method === "PUT") {
+    updatePrivileges(req, res);
+    return;
+  }
+
+
+
+
+  switch (route) {
+    case LOGIN:
+      loginRoute(req, res);
+      return;
+    case SIGNUP:
+      signupRoute(req, res);
+      return;
+    case PERMISSION:
+      updatePrivileges(req, res);
+      return;
+    case MAIN_ROOT:
+      mainRoute(req, res);
+      return;
+    case CREATE_EVENT:
+      createEvent(req, res);
+      return;
+    default:
+      res.statusCode = 404;
+      res.write(" API Path not found"); // build in js function, to convert json to a string
+      res.end();
+      return;
+  }
 });
 
 server.listen(port);
--- src/models/users.ts
+++ src/models/users.ts
@@ -1,19 +1,9 @@
 import * as mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema({
-    orderID: { type: mongoose.Schema.Types.ObjectId, required: true},
-    eventID: { type: mongoose.Schema.Types.ObjectId, required: true},
-    ticketType: { type: String, required: true},
-    ticketQuantity: { type: Number, required: true},
-    timeOfPurchase: { type: Date, default: Date.now, required: true}
-},{_id: false});
-
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true, minLength: 1 },
     password: { type: String, required: true,  minLength: 1},
-    permission: { type: String, required: true, enum:["W","M","A","U"]},
-    orders: { type: [orderSchema], required: false},
+    permission: { type: String,required: true, enum:["W","M","A"]},
 });
 
-// Create models from schemas and export them
 export default mongoose.model("User", userSchema);
